49c163c77ff6d1c2a72a4c9e873fe19c6a1400c4
anthony <anthonychirichella05@gmail.com>
2026-02-05 11:10:17 -0500

    first commit

--- src/monsters/Main.java
+++ src/monsters/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args){
         Monster m1 = new Water("Cthulhu");
         Monster m2 = new Undead("Frankenstien");
+
+        System.out.println("Monster 1 - " + m1.getName());
+        System.out.println("Monster 2 - " + m2.getName());
+
+
     }
 
 }
